65d0b811b9f4f0bde2971848e1d94706f5f46e8f
rebecca-mui <rebecca.mui@colostate.edu>
2012-04-22 23:09:44 -0600

    added code to call other components and do interative query, added list of servers

--- src/launcher/dns_client.cc
+++ src/launcher/dns_client.cc
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <fstream>
+#include <string>
+
 #include "../common/query_processor.h"
+#include "../common/io.h"
+
+using namespace std;
 
 void showUsageandExit()
 {
@@ -36,6 +41,13 @@ int main(int argc, char** argv)
 	int nAddresses = 0;
 	const char** entries;
 
+	// list of root servers on humboldt using dig . NS
+	int numServers = 9;
+	string rootServer[numServers] = {  "198.41.0.4", "192.228.79.201",   "192.33.4.12",
+									  "128.8.10.90", "192.203.230.10",   "192.5.5.241",
+									 "192.112.36.4",    "128.63.2.53", "192.36.148.17"};
+
+
 	// == parse command line arguments ==
 	if (argc != 2)
 		showUsageandExit();
@@ -46,14 +58,72 @@ int main(int argc, char** argv)
 	QueryProcessor* qp = (QueryProcessor*)malloc(sizeof(QueryProcessor));
 	qp->getDnsNameFormat((char*)hostName);
 
+
 	// ==> do some checking to verify valid hostname - in query processor?
 
-
+	// == create query ==
+	Message queryMessage = qp->getDnsQuery(hostName);
 
 	// ==> start dns lookup...
+	UDPClient* client;
+	int rootServerIndex = 0;
+	int maxTimeouts = 3;
+	int numTimeouts = 0;
+	bool queryCompleted = false;
+	while (!queryCompleted)
+	{
+		try
+		{
+			// if exists, need to destroy old client first??
+
+			client = new UDPClient(rootServer[rootServerIndex]);
+		}
+		catch (...)
+		{
+			rootServerIndex++;
+			if (rootServerIndex == numServers)
+				break;
+
+			continue;
+		}
+
+		client->sendRequest(queryMessage);
+
+		try
+		{
+			Message message = client->receiveResponse();
+		}
+		catch (int status)
+		{
+			// error message from client
+			// if timeout, try again
+			if (status == 0)
+			{
+				numTimeouts++;
+				if (numTimeouts == maxTimeouts)  // go to next root server
+				{
+					numTimeouts = 0;
+					rootServerIndex++;
+					if (rootServerIndex == numServers)
+						break;
+				}
+
+				continue;
+			}
+		}
+		catch (...)
+		{
+		}
 
 
+		//==>parse message
 
+
+		//==> submit new queries if needed
+
+
+		queryCompleted = true;  // set if no further queries
+	}
 
 
 	// for testing...
