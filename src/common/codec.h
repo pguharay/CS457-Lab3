175c7581cde3e2fea17bc1cf9acce12f7f2d9a54
PRATIK GUHARAY <pratik.guha@gmail.com>
2012-05-02 12:02:05 -0700

    - Adding the changed header signature

--- src/common/codec.h
+++ src/common/codec.h
@@ -5,7 +5,7 @@ using namespace std;
 #ifndef CODEC_H_
 #define CODEC_H_
 
-string encode(unsigned char const* rawBinaryData, unsigned int dataLength);
+string encode(char* rawBinaryData, unsigned int dataLength);
 void mapBinaryToAscii(unsigned char* binaryBlock,  unsigned char* asciiBlock);
 
 #endif
